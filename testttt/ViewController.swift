807fe4ded9436912e78ff23f86cee7f85a54d685
Takemi Watanuki <watakemi725@Takemi-no-MacBook-Pro.local>
2015-02-28 10:31:32 +0900

    add new function

---
+++ testttt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testttt
+//
+//  Created by Takemi Watanuki on 2015/02/28.
+//  Copyright (c) 2015年 watakemi725. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
